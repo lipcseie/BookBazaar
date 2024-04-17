320695686bf58de9b10fd5ccf4a393770e8ee30e
lipcseie <lipcsei.e@gmail.com>
2024-04-17 17:00:41 +0200

    add category table

--- Bazaar/BookBazaar/Controllers/CategoryController.cs
+++ Bazaar/BookBazaar/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookBazaar.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookBazaar.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
