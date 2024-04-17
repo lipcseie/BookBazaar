c2d39ea9c8896266902ec5ca41ea1b1c486990e7
lipcseie <lipcsei.e@gmail.com>
2024-04-17 15:53:06 +0200

    add category table to Db

---
+++ Bazaar/BookBazaar/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookBazaar.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookBazaar.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- Bazaar/BookBazaar/Models/Category.cs
+++ Bazaar/BookBazaar/Models/Category.cs
@@ -1,6 +1,16 @@
-﻿namespace BookBazaar.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookBazaar.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DispkayOrder { get; set; }
+
     }
 }
