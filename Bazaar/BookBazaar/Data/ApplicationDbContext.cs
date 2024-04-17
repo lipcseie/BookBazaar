caa066a5ddd7416c2915c2ed745a460764341261
lipcseie <lipcsei.e@gmail.com>
2024-04-17 16:24:58 +0200

    add SeedCategoryTable migration

--- Bazaar/BookBazaar/Data/ApplicationDbContext.cs
+++ Bazaar/BookBazaar/Data/ApplicationDbContext.cs
@@ -11,5 +11,15 @@ namespace BookBazaar.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Cooking", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Fantasy", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Self-Help", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Adventure", DisplayOrder = 4 }
+                );
+        }
     }
 }
