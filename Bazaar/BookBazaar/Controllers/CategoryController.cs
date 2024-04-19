44d3733478fb0b7d8ab0e1cd383db0f55a81ea07
lipcseie <lipcsei.e@gmail.com>
2024-04-19 16:46:24 +0200

    add delete to the controller and delete view

--- Bazaar/BookBazaar/Controllers/CategoryController.cs
+++ Bazaar/BookBazaar/Controllers/CategoryController.cs
@@ -56,17 +56,43 @@ namespace BookBazaar.Controllers
         [HttpPost]
         public IActionResult Edit(Category category)
         {
-            if (category.Name == category.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "DisplayOrder cannot match the Name");
-            }
             if (ModelState.IsValid)
             {
-                _context.Categories.Add(category);
+                _context.Categories.Update(category);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
