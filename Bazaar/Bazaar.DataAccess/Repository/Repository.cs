1bb2c05a4ad0efcc6d5a7ba773e7829487dd54a1
lipcseie <lipcsei.e@gmail.com>
2024-05-23 16:13:41 +0200

    implement RemoveRange method

--- Bazaar/Bazaar.DataAccess/Repository/Repository.cs
+++ Bazaar/Bazaar.DataAccess/Repository/Repository.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Bazaar.DataAccess.Data;
+using Bazaar.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bazaar.DataAccess.Repository
 {
-    internal class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+           dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
     }
 }
