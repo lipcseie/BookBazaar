13ca274073aac353beced476d4493faa5a07288d
lipcseie <lipcsei.e@gmail.com>
2024-06-18 16:56:45 +0200

    add product image folder

--- Bazaar/BookBazaar/Areas/Admin/Controllers/ProductController.cs
+++ Bazaar/BookBazaar/Areas/Admin/Controllers/ProductController.cs
@@ -10,10 +10,12 @@ namespace BookBazaar.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -51,6 +53,19 @@ namespace BookBazaar.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath,fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productViewModel.Product.ImageUrl = @"\images\product\" + fileName;
+                }   
                 _unitOfWork.Product.Add(productViewModel.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "PRODUCT CREATED SUCCESFULLY ";
